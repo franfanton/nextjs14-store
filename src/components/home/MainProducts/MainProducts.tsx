fff257a16fbcf4801a7aaef5df6dc44cdfc196e0
franfanton <franfanton92@gmail.com>
2024-01-13 17:38:04 -0300

    integrate Shopify API and create a getProducts

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,4 +1,20 @@
-export const MainProducts = () => {
+const getProducts = async () => {
+  const response = await fetch(
+    `${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,
+    {
+      headers: new Headers({
+        "X-Shopify-Access-Token": process.env.SHOPIFY_API_KEY || "",
+      }),
+    }
+  );
+  const data = response.json();
+
+  return data;
+};
+
+export const MainProducts = async () => {
+  const products = await getProducts();
+
   return (
     <section>
       <h1>MainProducts</h1>
