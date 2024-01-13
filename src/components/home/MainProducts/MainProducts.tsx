25a74b6eb8b6a7371dff3a39d19f870e6047be8b
franfanton <franfanton92@gmail.com>
2024-01-13 18:21:34 -0300

    add styles mainProducst and render map products

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,23 +1,80 @@
-const getProducts = async () => {
-  const response = await fetch(
-    `${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,
-    {
-      headers: new Headers({
-        "X-Shopify-Access-Token": process.env.SHOPIFY_API_KEY || "",
-      }),
-    }
-  );
-  const data = response.json();
+import Image from "next/image";
+import styles from "./MainProducts.module.sass";
 
-  return data;
+interface Product {
+  id: number;
+  title: string;
+  body_html: string;
+  vendor: string;
+  product_type: string;
+  created_at: string;
+  handle: string;
+  updated_at: string;
+  published_at: string;
+  template_suffix: null | string;
+  published_scope: string;
+  tags: string;
+  status: string;
+  admin_graphql_api_id: string;
+  variants: Array<{
+    /* Detalles de la variante */
+  }>; // Necesitarías definir el tipo para las variantes
+  options: Array<{
+    /* Detalles de las opciones */
+  }>; // Necesitarías definir el tipo para las opciones
+  images: Array<{
+    /* Detalles de las imágenes */
+    src: string;
+  }>; // Necesitarías definir el tipo para las imágenes
+  image: {
+    id: number;
+    alt: null | string;
+    position: number;
+    product_id: number;
+    created_at: string;
+    updated_at: string;
+    admin_graphql_api_id: string;
+    width: number;
+    height: number;
+    src: string;
+    variant_ids: number[];
+  };
+}
+
+const getProducts = async () => {
+  try {
+    const response = await fetch(
+      `${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,
+      {
+        headers: new Headers({
+          "X-Shopify-Access-Token": process.env.SHOPIFY_API_KEY || "",
+        }),
+      }
+    );
+    const { products } = await response.json();
+    return products;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const MainProducts = async () => {
   const products = await getProducts();
 
   return (
-    <section>
-      <h1>MainProducts</h1>
+    <section className={styles.MainProducts}>
+      <h3>✨ New products released!</h3>
+      <div className={styles.MainProducts__grid}>
+        {products?.map((product: Product) => {
+          const imageSrc = product.images[0].src;
+          return (
+            <article key={product.id}>
+              <p>{product.title}</p>
+              <Image src={imageSrc} fill alt={product.title} loading="eager" />
+            </article>
+          );
+        })}
+      </div>
     </section>
   );
 };
